aaa6b6bb9af2b0a38549102000eb8b8cc2a8f4e8
EdinK1 <edinkaymakchi@gmail.com>
2020-01-28 19:41:31 -0800

    add dev script

---
+++ client/dist/templates/Users/nevermore/code/red-talent-2020/client/src/pages/about.d6af6598.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{52:function(n,e,t){"use strict";t.r(e);var u=t(0),a=t.n(u);e.default=function(){return a.a.createElement("p",null,"about")}}}]);
