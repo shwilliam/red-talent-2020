a54e45fca7ecf448b0530159891ebb8c12bf7379
EdinK1 <edinkaymakchi@gmail.com>
2020-02-02 00:36:11 -0800

    export components properly

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -8,6 +8,11 @@ import SubTitle from './Subtitle'
 import Collapsible from './Collapsible'
 import Image from './Image'
 import Banner from './Banner'
+import Header from './Header'
+import Input from './Input'
+import GraduatesLabel from './GraduatesLabel'
+import Label from './Label'
+import Paragraph from './Paragraph'
 
 export {
   Header,
