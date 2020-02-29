8b50096c65829334ee012c640305061cb7027f22
EdinK1 <edinkaymakchi@gmail.com>
2020-02-28 16:03:10 -0800

    fix: change banner image position

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import BannerImg from '../../assets/images/about-banner.jpg'
 
-const StyledImg = styled.img`
+const StyledImg = styled.section`
   position: absolute;
   top: 0;
   left: 0;
@@ -11,18 +11,15 @@ const StyledImg = styled.img`
   width: 100%;
   z-index: -1;
   height: 260px;
-  background: linear-gradient(
-      rgba(0, 0, 0, 0.35) 0,
-      rgba(0, 0, 0, 0.35)
-    ),
+  background: linear-gradient(rgba(0, 0, 0, 0.35) 0, rgba(0, 0, 0, 0.35)),
     url(${BannerImg});
-  background-position: 50%;
+  background-position: 0 0;
   background-size: cover;
   @media only screen and (min-width: 768px) {
     height: 575px;
   }
 `
 
-const Banner = ({image}) => <StyledImg src={image} />
+const Banner = () => <StyledImg />
 
 export default Banner
